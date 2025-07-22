2aa6dfdea6b8dc42bb205c233cac3d696dbc9688
Ravi kumar <rkconsultancy34@gmail.com>
2025-07-22 12:27:26 +0530

    script remove

--- scripts/00-deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-composer install --no-dev --optimize-autoloader
-php artisan key:generate --force
-php artisan config:cache
-php artisan route:cache
-php artisan migrate --force
