ca3baf8c5998120e0291c051d4b3e5a6c2b4734d
Ravi kumar <rkconsultancy34@gmail.com>
2025-07-22 22:41:11 +0530

    script add

--- Dockerfile
+++ Dockerfile
@@ -26,3 +26,7 @@ EXPOSE 8000
 CMD php artisan serve --host=0.0.0.0 --port=8000
 
 
+COPY scripts/00-laravel-deploy.sh /deploy.sh
+RUN chmod +x /deploy.sh
+
+CMD ["/deploy.sh"]
---
+++ scripts/00-laravel-deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+echo "Installing composer and dependencies…"
+/usr/bin/composer install --no-dev --optimize-autoloader
+
+echo "Generating app key…"
+php artisan key:generate --force
+
+echo "Caching config and routes…"
+php artisan config:cache
+php artisan route:cache
+
+echo "Running migrations…"
+php artisan migrate --force
+
+echo "Starting Laravel server…"
+php artisan serve --host=0.0.0.0 --port=8000
